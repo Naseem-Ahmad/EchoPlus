4b8dbb1720f87ad14e59bf2af002f4bd8efb5d47
Naseem Ahmad <naseemahmad1998@gmail.com>
2025-11-22 22:03:02 +0530

    pro url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:7080/api",  // your API https://determined-rejoicing-production.up.railway.app/
+  baseURL: "https://determined-rejoicing-production.up.railway.app/",  // your API https://determined-rejoicing-production.up.railway.app/
   
 });
 
